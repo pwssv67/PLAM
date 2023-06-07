044d1c7028a3a88158e34c2b7f3fe5ecfccdb76f
esmolianinov <pwssv67@gmail.com>
2023-06-07 13:54:47 +0200

    use rootProject for storing Configuration

--- plam/src/main/kotlin/com/pwssv67/plam/PlamPlugin.kt
+++ plam/src/main/kotlin/com/pwssv67/plam/PlamPlugin.kt
@@ -6,16 +6,28 @@ import org.gradle.api.Project
 
 class PlamPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-            val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
-            androidComponents.onVariants { variant ->
-                val task = project.tasks.register(variant.name + "CheckHierarchy", CheckHierarchyTask::class.java)
+        try {
+            attachTasksToAndroidFlavorTasks(project)
+        } catch (e: Throwable) {
+            if (e is ClassNotFoundException || e is NoClassDefFoundError) {
+                project.logger.lifecycle("Android Gradle Plugin seems to be missing in ${project.name}. For this module plugin won't check anything.")
+            } else {
+                throw e
+            }
+        }
+    }
 
-                project.tasks.whenTaskAdded {
-                    if (name.contains(variant.name, ignoreCase = true)) {
-                        dependsOn(task)
-                    }
+    private fun attachTasksToAndroidFlavorTasks(project: Project) {
+        val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
+        androidComponents.onVariants { variant ->
+            val task = project.tasks.register(variant.name + "CheckHierarchy", CheckHierarchyTask::class.java)
+
+            project.tasks.whenTaskAdded {
+                if (name.contains(variant.name, ignoreCase = true)) {
+                    dependsOn(task)
                 }
             }
+        }
     }
 }
 
