c2f2650822f68bfe363f45fc6e0550dec0f3f5e2
esmolianinov <pwssv67@gmail.com>
2023-06-09 16:15:43 +0200

    update version

--- samples/simple modular project/build.gradle.kts
+++ samples/simple modular project/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.application") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.0.0" apply false
-    id("io.github.pwssv67.plam") version "0.2.2" apply false
+    id("io.github.pwssv67.plam") version "0.3.0" apply false
 }
 
 plam {
